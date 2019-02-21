e8e900e0335ef280f9524a8942760a101be547ec
mkdoh3 <mkdoherty3@GMAIL.COM>
2019-02-21 16:32:47 -0600

    debugging access to authorized site by calendly post request

--- server.js
+++ server.js
@@ -2,20 +2,7 @@ require("dotenv").config();
 const express = require("express");
 const routes = require("./routes");
 const wwwhisper = require("connect-wwwhisper");
-const sheets = require("./utils/sheetsUtils");
 const app = express();
-app.post("/api/sheets/hook", (req, res) => {
-  console.log("REQUEST FROM CALENDLY ---------------->  ", req);
-  if (req.body.event === "invitee.created") {
-    sheets.createSession(req.body, true).then(() => {
-      res.status(200).send("OK");
-    });
-  } else {
-    sheets.deleteSession(req.body.payload.event.uuid).then(() => {
-      res.status(200).send("OK");
-    });
-  }
-});
 app.use(wwwhisper(false)); //false removes the logout iframe - login is maintained by cookies
 require("./utils/mailer");
 const PORT = process.env.PORT || 3001;
