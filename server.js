1cf7f72a47952d3543481b7a4b6ffc8198674ca1
mkdoh3 <mkdoherty3@GMAIL.COM>
2019-02-21 15:59:03 -0600

    debugging access to authorized site by calendly post request

--- server.js
+++ server.js
@@ -3,6 +3,9 @@ const express = require("express");
 const routes = require("./routes");
 const wwwhisper = require("connect-wwwhisper");
 const app = express();
+app.post("/api/sheets/hook", (req, res) => {
+  console.log(req.url);
+});
 app.use(wwwhisper(false)); //false removes the logout iframe - login is maintained by cookies
 require("./utils/mailer");
 const PORT = process.env.PORT || 3001;
