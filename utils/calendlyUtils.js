d5178d9095b3efdd341eee3ee734adde8d273380
mkdoh3 <mkdoherty3@GMAIL.COM>
2019-02-22 10:45:01 -0600

    changed FilteredHookData to properly show invitee start time

--- utils/calendlyUtils.js
+++ utils/calendlyUtils.js
@@ -1,8 +1,8 @@
 const moment = require("moment");
 function FilteredHookData(data) {
-  this.sessionDate = moment(data.event.start_time).format("YYYY-MM-DD");
+  this.sessionDate = moment(data.event.invitee_start_time).format("YYYY-MM-DD");
   this.topicsCovered = data.questions_and_responses["1_response"];
-  this.studentSessionTime = data.event.start_time_pretty.split(" -")[0];
+  this.studentSessionTime = data.event.invitee_start_time_pretty.split(" -")[0];
   this.studentEmail = data.invitee.email;
   this.sessionId = data.event.uuid;
 }
