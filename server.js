d601477974a3d24a87a0c70fca6df83af9680215
mkdoh3 <mkdoherty3@GMAIL.COM>
2019-02-22 11:54:45 -0600

    debugging access to authorized site by calendly post request

--- server.js
+++ server.js
@@ -2,25 +2,14 @@ require("dotenv").config();
 const express = require("express");
 const routes = require("./routes");
 const wwwhisper = require("connect-wwwhisper");
-// const sheets = require("./utils/sheetsUtils");
 const app = express();
-app.use(express.urlencoded({ extended: true }));
-app.use(express.json());
-app.use(wwwhisper());
-// app.post("/api/sheets/hook", (req, res) => {
-//   if (req.body.event === "invitee.created") {
-//     sheets.createSession(req.body, true).then(() => {
-//       res.status(200).send("OK");
-//     });
-//   } else {
-//     sheets.deleteSession(req.body.payload.event.uuid).then(() => {
-//       res.status(200).send("OK");
-//     });
-//   }
-// });
+
+app.use(wwwhisper()); //false removes the logout iframe - login is maintained by cookies
 require("./utils/mailer");
 const PORT = process.env.PORT || 3001;
 
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
 if (process.env.NODE_ENV === "production") {
   app.use(express.static("client/build"));
 }
