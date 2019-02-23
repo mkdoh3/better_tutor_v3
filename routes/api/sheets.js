c1936504815fbb9720774caa10242018cbb61cea
mkdoh3 <mkdoherty3@GMAIL.COM>
2019-02-23 13:52:30 -0600

    calendly hook all fixed up

--- routes/api/sheets.js
+++ routes/api/sheets.js
@@ -3,7 +3,7 @@ const sheetsController = require("../../controllers/sheetsController");
 
 router.route("/session").post(sheetsController.createSession);
 router.route("/update").post(sheetsController.updateRows);
-// router.route("/hook").post(sheetsController.handleHook);
+router.route("/hook").post(sheetsController.handleHook);
 
 router
   .route("/tab/:tab")
