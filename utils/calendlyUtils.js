3de0fec26f6a88621d479adf23f2de653ade8878
mkdoh3 <mkdoherty3@GMAIL.COM>
2019-02-27 11:16:59 -0600

    debugging calendly date parsing error

--- utils/calendlyUtils.js
+++ utils/calendlyUtils.js
@@ -1,6 +1,8 @@
 const moment = require("moment");
 function FilteredHookData(data) {
-  this.sessionDate = moment(data.event.invitee_start_time).format("YYYY-MM-DD");
+  this.sessionDate = moment(
+    data.event.invitee_start_time_pretty.split(" -")[1]
+  ).format("YYYY-MM-DD");
   this.topicsCovered = data.questions_and_responses["1_response"];
   this.studentSessionTime = data.event.invitee_start_time_pretty.split(" -")[0];
   this.studentEmail = data.invitee.email;
